204269e461075d30bc44ee10e2555d1674fc0d37
Devke <chriseke13@gm>
2021-02-10 09:03:02 -0600

    Updated Handler file

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -72,7 +72,7 @@ module.exports.getAuthURL = async () => {
       client_secret,
       redirect_uris[0]
     );
-    const access_token = decodeURIComponent(`${event.pathParameters.code}`);
+    const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
     oAuth2Client.setCredentials({access_token});
     return new Promise((resolve, reject) => {
       calendar.events.list(
