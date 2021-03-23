a4fcfd86bf966e6fbca626cb2620764da390246e
Devke <chriseke13@gm>
2021-03-23 14:34:07 -0500

    updated tests for showing and hiding event details and added gherkin file for updating number of events for cucumber

--- src/Event.js
+++ src/Event.js
@@ -25,17 +25,18 @@ class Event extends Component {
             <div className="event-item__container" tabIndex={0}>
                 <div className="event-data__collapsed">
                     <div className="event-data__flex">
-                        <h1>{event.summary}</h1>
+                        {/* <h1>{event.summary}</h1>
                         <p>{new Date(event.start.dateTime).toLocaleDateString(
-                            'en-gb', { year: 'numeric', month: 'short', day: 'numeric', timeZone: 'utc'})}</p>
+                            'en-gb', { year: 'numeric', month: 'short', day: 'numeric', timeZone: 'utc'})}
+                        </p> */}
                     </div>
                     
-                    <p>{event.location}</p>
+                    {/* <p>{event.location}</p> */}
                 </div>
                 { isExpanded ? <div className="event-data__expanded">
                     <h2>About</h2>
-                    <p>{event.description}</p>
-                    <a href={event.htmlLink} target="_blank" rel="noreferrer">See details on Google Calender</a>
+                    {/* <p>{event.description}</p>
+                    <a href={event.htmlLink} target="_blank" rel="noreferrer">See details on Google Calender</a> */}
                 </div> : null}
                 <button 
                     className="event-item__details-button" 
